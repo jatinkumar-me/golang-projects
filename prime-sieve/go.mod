ee78c08a72a4f756336c1ae67565707f495b792c
jatinkumar-me <jatin208015@gmail.com>
2024-03-29 22:45:35 +0530

    prime sieve example
    A concurrent program for generating prime numbers

---
+++ prime-sieve/go.mod
@@ -0,0 +1,3 @@
+module prime-sieve
+
+go 1.21.6
---
+++ prime-sieve/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+// A concurrent programming for generating prime numbers using goroutines and channels
+
+// generator does nothing but sends numbers from 2 to limit to write only channel numChan
+func generator(limit int, src chan<- int) {
+	for i := 2; i < limit; i++ {
+		src <- i
+	}
+
+    close(src)
+}
+
+func filter(src <-chan int, dest chan<- int, prime int) {
+    for i := range(src) {
+        if i % prime != 0 {
+            dest <- i
+        }
+    }
+
+    close(dest)
+}
+
+func sieve(limit int) {
+    src := make(chan int)
+    go generator(limit, src)
+
+    for {
+        prime, ok := <- src
+        if !ok {
+            break
+        }
+
+        newSrc := make(chan int)
+        go filter(src, newSrc, prime)
+
+        src = newSrc
+
+        fmt.Print(prime, " ")
+    }
+}
+
+func main() {
+    sieve(100)
+}
