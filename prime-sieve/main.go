c55c9359cd3cfc9b675dea30ae8cc63ff6b2f3f4
jatinkumar-me <jatin208015@gmail.com>
2024-03-31 11:47:13 +0530

    format changes

--- prime-sieve/main.go
+++ prime-sieve/main.go
@@ -1,47 +1,46 @@
+// A concurrent programming for generating prime numbers using goroutines and channels
 package main
 
 import "fmt"
 
-// A concurrent programming for generating prime numbers using goroutines and channels
-
-// generator does nothing but sends numbers from 2 to limit to write only channel numChan
+// generator sends numbers from 2 to limit to src channel
 func generator(limit int, src chan<- int) {
 	for i := 2; i < limit; i++ {
 		src <- i
 	}
 
-    close(src)
+	close(src)
 }
 
 func filter(src <-chan int, dest chan<- int, prime int) {
-    for i := range(src) {
-        if i % prime != 0 {
-            dest <- i
-        }
-    }
+	for i := range src {
+		if i%prime != 0 {
+			dest <- i
+		}
+	}
 
-    close(dest)
+	close(dest)
 }
 
 func sieve(limit int) {
-    src := make(chan int)
-    go generator(limit, src)
+	src := make(chan int)
+	go generator(limit, src)
 
-    for {
-        prime, ok := <- src
-        if !ok {
-            break
-        }
+	for {
+		prime, ok := <-src
+		if !ok {
+			break
+		}
 
-        newSrc := make(chan int)
-        go filter(src, newSrc, prime)
+		newSrc := make(chan int)
+		go filter(src, newSrc, prime)
 
-        src = newSrc
+		src = newSrc
 
-        fmt.Print(prime, " ")
-    }
+		fmt.Print(prime, " ")
+	}
 }
 
 func main() {
-    sieve(100)
+	sieve(100)
 }
